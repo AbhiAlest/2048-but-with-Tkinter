a1ba586f7a433517ac7badbbf668ab8239182c84
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-11 18:06:26 -0500

    Create c.py

---
+++ c.py
@@ -0,0 +1,50 @@
+gc = "#a39489"
+etc = "#c2b3a9"
+slf = ("Verdana", 20)
+sf = ("Helvetica", 32, "bold")
+gof = ("Helvetica", 48, "bold")
+gofc = "#ffffff"
+wb = "#ffcc00"
+lbg = "#a39489"
+
+tc = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "#fad74d"
+}
+
+tnc = {
+    2: "#695c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    512: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
+
+tnf = {
+    2: ("Helvetica", 55, "bold"),
+    4: ("Helvetica", 55, "bold"),
+    8: ("Helvetica", 55, "bold"),
+    16: ("Helvetica", 50, "bold"),
+    32: ("Helvetica", 50, "bold"),
+    64: ("Helvetica", 50, "bold"),
+    128: ("Helvetica", 45, "bold"),
+    256: ("Helvetica", 45, "bold"),
+    512: ("Helvetica", 45, "bold"),
+    1024: ("Helvetica", 40, "bold"),
+    2048: ("Helvetica", 40, "bold")
+}
